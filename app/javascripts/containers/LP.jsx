3b19ba63f80cb16500377a6597a025dc30057228
Ryoya Tamura <simochee@gmail.com>
2017-12-09 21:52:41 +0900

    Set GetStarted component to Container

--- app/javascripts/containers/LP.jsx
+++ app/javascripts/containers/LP.jsx
@@ -5,6 +5,7 @@ import Hero from '../components/LP/Hero';
 import Pickup from '../components/LP/Pickup';
 import Feature from '../components/LP/Feature';
 import Help from '../components/LP/Help';
+import GetStrated from '../components/LP/GetStarted';
 
 class LP extends Component {
   render () {
@@ -14,6 +15,7 @@ class LP extends Component {
         <Pickup />
         <Feature />
         <Help />
+        <GetStrated />
       </div>
     );
   }
