d2282af7b3dcd715d01af38652ba2c945db853a8
Ryoya Tamura <simochee@gmail.com>
2017-12-09 21:38:01 +0900

    Set Help component to Container

--- app/javascripts/containers/LP.jsx
+++ app/javascripts/containers/LP.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import Hero from '../components/LP/Hero';
 import Pickup from '../components/LP/Pickup';
 import Feature from '../components/LP/Feature';
+import Help from '../components/LP/Help';
 
 class LP extends Component {
   render () {
@@ -12,6 +13,7 @@ class LP extends Component {
         <Hero />
         <Pickup />
         <Feature />
+        <Help />
       </div>
     );
   }
